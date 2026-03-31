654b28b5ffbb5f528f7738b5002f9a9e3f141b9f
Intuchhhh <act22117@gmail.com>
2026-04-01 04:25:52 +0700

    Main menu and Ui updated

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    private const string GameSceneName = "Level";
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(OnStartClicked);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Quit();
+        }
+    }
+
+    /// <summary>Loads the main game scene.</summary>
+    public void OnStartClicked()
+    {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    /// <summary>Exits the application.</summary>
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
+
+
+
